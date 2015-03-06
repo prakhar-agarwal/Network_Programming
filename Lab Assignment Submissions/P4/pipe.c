dc537ef4d7732f3cfd095598b04721b749aec345
ubuntu <prakharagarwal2511@gmail.com>
2015-03-06 15:23:50 +0530

    Revert "Network Programming Labs"
    This reverts commit 7bbaeedc9472624c18c7d6f96b9904f85d5443a3.

--- Lab Assignment Submissions/P4/pipe.c
+++ Lab Assignment Submissions/P4/pipe.c
@@ -5,61 +5,64 @@
 #include<stdlib.h>
 #include<string.h>
 
-main(int argc, char *argv[]){
-    int i,fd1r[2],fd1w[2],fd2r[2],fd2w[2],cpid[2],pid,len;
+main(int argc, char **argv){
+	
+    int p1[2],p2[2],p3[2],p4[2],child[2],pid,len;
     char buf[256];
-    pipe(fd1r);                 //Pipe to read from Child 1
-    pipe(fd1w);                 //Pipe to write to Child 1
-    pipe(fd2r);                 //Pipe to read from Child 2
-    pipe(fd2w);                 //Pipe to write to Child 2
-    for(i=0;i<2;i++){
-        pid = fork();
-        if(pid==0){             //Child breaks out of loop
+    pipe(p1);                 
+    pipe(p2);                 
+    pipe(p3);                 
+    pipe(p4);                 
+    int i=0;
+    while (i<2){
+            pid = fork();
+        if(pid==0){            
             break;
         }
-        cpid[i] = pid;          //Parent copies child pid into array
+        child[i] = pid;
+        i++;
     }
-
-    if(pid == 0){               //Child
-        if(i==0){               //Child 1
-            close(fd1r[0]);
-            close(fd1w[1]);
-            while(read(fd1w[0],&buf,sizeof(buf))){
-                strcat(buf,"C1");            //Child 1 adds " C1" to string
-                write(fd1r[1],&buf,sizeof(buf));
+    if(pid == 0){               
+        if(i==0){               
+            close(p1[0]);
+            close(p2[1]);
+            while(read(p2[0],&buf,sizeof(buf))){
+                strcat(buf,"C1");          
+                write(p1[1],&buf,sizeof(buf));
             }
-            close(fd1r[1]);
-            close(fd1w[0]);
+            close(p1[1]);
+            close(p2[0]);
         }
-        if(i==1){               //Child 2
-            close(fd2r[0]);
-            close(fd2w[1]);
-            while(read(fd2w[0],&buf,sizeof(buf))){
-                strcat(buf,"C2");            //Child 2 adds " C2" to string
-                write(fd2r[1],&buf,sizeof(buf));
+        if(i==1){               
+            close(p3[0]);
+            close(p4[1]);
+            while(read(p4[0],&buf,sizeof(buf))){
+                strcat(buf,"C2");
+                write(p3[1],&buf,sizeof(buf));
             }
-            close(fd2r[1]);
-            close(fd2w[0]);
+            close(p3[1]);
+            close(p4[0]);
         }
     }
-    else{                       //Parent
-        close(fd1w[0]);
-        close(fd1r[1]);
-        close(fd2w[0]);
-        close(fd2r[1]);
+    else{
+        close(p2[0]);
+        close(p1[1]);
+        close(p4[0]);
+        close(p3[1]);
         memset(buf,'\0',sizeof(buf));
-        while(read(0,&buf,sizeof(buf))){        //Read from stdin till user closes pipe by Ctrl+D
-            buf[strlen(buf)-1]='\0';            //End the input string before the endline character
-            write(fd1w[1],&buf,sizeof(buf));
-            read(fd1r[0],&buf,sizeof(buf));
-            write(fd2w[1],&buf,sizeof(buf));
-            read(fd2r[0],&buf,sizeof(buf));
-            printf("%s\n",buf);
+        while(read(0,&buf,sizeof(buf))){        
+            buf[strlen(buf)-1]='\0';           
+            write(p2[1],&buf,sizeof(buf));
+            read(p1[0],&buf,sizeof(buf));
+            write(p4[1],&buf,sizeof(buf));
+            read(p3[0],&buf,sizeof(buf));
+            printf("%s\n\n",buf);
             memset(buf,'\0',sizeof(buf));
         }
-        close(fd1w[1]);
-        close(fd1r[0]);
-        close(fd2w[1]);
-        close(fd2r[0]);
+        close(p2[1]);
+        close(p1[0]);
+        close(p4[1]);
+        close(p3[0]);
+ 
     }
 }
